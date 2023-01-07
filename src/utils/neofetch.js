b2a42b394dc429572248111609107d8f9ae1c255
Alberto <info@peralstudio.com>
2023-01-07 18:39:21 +0100

    Change neofetch

--- src/utils/neofetch.js
+++ src/utils/neofetch.js
@@ -1,10 +1,10 @@
 const neofetch = `
-<span class="style4">Alberto</span>@<span class="style4">portfolio</span>
------------------
+<span class="style4">AlbertoPeral</span>@<span class="style4">portfolio</span>
+----------------------
 <span class="style2"></span><span class="style4">Name: </span><span class="style1">Alberto Peral</span>
-<span class="style2"></span><span class="style4">Education: </span><span class="style1">ASOCIACIÓN ESPAÑOLA DE PROGRAMADORES 
-INFORMÁTICOS</span>
-<span class="style2"></span><span class="style4">Interests: </span><span class="style1">Front-End / Full Stack / JavaScript</span>
+<span class="style2"></span><span class="style4">Age: </span><span class="style1">36</span>
+<span class="style2"></span><span class="style4">Education: </span><span class="style1">ASOCIACIÓN ESPAÑOLA DE PROGRAMADORES INFORMÁTICOS</span>
+<span class="style2"></span><span class="style4">Interests: </span><span class="style1">Front-End / Full Stack / React / React Native / JavaScript</span>
 <span class="style2"></span><span class="style4">Socials: </span><span class="style2"><a target="_blank" href="https://www.linkedin.com/in/alberto-peral/">LinkedIn</a> <span class="style1">/</span> <a target="_blank" href="https://github.com/PeralStudio/">Github</a></span>
 <span class="style2"></span><span class="style4">Contacts: </span><span class="style2"><a target="_blank" href="mailto:info@peralstudio.com">Email</a>
 
